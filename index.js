824e2d06885c8595b13d8b103a00a1181b97dc71
rachel-taylor <taylor.d.rachel@gmail.com>
2021-02-26 23:51:46 -0800

    commit tree fef6e7d10e35c38697701a6e29dfdd16f9bd090e

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  displayMessage(greet())
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(str) {
+  let hour = str.split(':')[0];
+  if (hour < 0) throw new RangeError("Sorry, that's not a valid time!");
+  if (hour < 12) return "Good Morning";
+  if (hour > 17) return "Good Evening";
+  return "Good Afternoon";
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
